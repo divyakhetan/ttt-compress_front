e9aae69c2338aee5d37cc415111f3eaf911a9221
divyakhetan <divyakhetan1209@gmail.com>
2020-08-15 21:53:01 +0530

    Updates

---
+++ precache-manifest.13fc08e373c4f5a5ede14d6bec437426.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6efd8d2ab8d91f9d6d7caeb4d0275db3",
+    "url": "/ttt-compress_front/index.html"
+  },
+  {
+    "revision": "dcba8468de1869f83b9e",
+    "url": "/ttt-compress_front/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a3af0d968417f916150",
+    "url": "/ttt-compress_front/static/js/2.4519e375.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ttt-compress_front/static/js/2.4519e375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcba8468de1869f83b9e",
+    "url": "/ttt-compress_front/static/js/main.b5819503.chunk.js"
+  },
+  {
+    "revision": "992a6ad34608bf08f57c",
+    "url": "/ttt-compress_front/static/js/runtime-main.aff075da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ttt-compress_front/static/media/logo.5d5d9eef.svg"
+  }
+]);
